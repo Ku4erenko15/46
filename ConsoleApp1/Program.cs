f28566140cb5d93ae793e587ffa3b6953baf1267
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-06-05 11:00:04 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        enum Days
+        {
+            Sat, Sun, Mon, Tue, Wed, Thu, Fri
+        }
+        static void Main(string[] args)
+        {
+            foreach (var month in Enum.GetValues(typeof(Days)))
+            {
+                Console.WriteLine(month.ToString() + " имеет значение " + (int)month);
+            }
+            Console.Read();
+
+
+            
+        }
+    }
+}
