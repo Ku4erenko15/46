2f781edd9a8f5d50161c9ac0aed904cefd0859e6
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-06-05 10:59:43 +0300

    Add files via upload

---
+++ 46.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _46._1
+{
+    internal class Program
+    {
+        enum Month { январь, февраль, март, апрель, май, июнь, июль, август, сентябрь, октябрь, ноябрь, декабрь}
+        static void Main(string[] args)
+        {
+            foreach (var month in Enum.GetValues(typeof(Month)))
+            {
+                Console.WriteLine((int)month+" "+month.ToString());
+            }
+            Console.Read();
+
+
+
+        }
+    }
+}
